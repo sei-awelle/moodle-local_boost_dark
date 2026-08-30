3b18d1532df0c5d61497f65f1effd92144d0f507
Eduardo Kraus <kraus@eduardokraus.com>
2026-08-30 15:14:23 -0300

    add PT-BR

---
+++ lang/pt_br/local_boost_dark.php
@@ -0,0 +1,73 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * lang PT-BR file
+ *
+ * @package   local_boost_dark
+ * @copyright 2024 Eduardo Kraus {@link https://eduardokraus.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['boost_dark:manage'] = 'Boost Dark';
+$string['bs_black'] = 'Cor preta';
+$string['bs_black_desc'] = 'Cor preta pura';
+$string['bs_gray_100'] = 'Cinza 100';
+$string['bs_gray_1000'] = 'Cinza 1000';
+$string['bs_gray_1000_desc'] = 'Tom de cinza muito escuro';
+$string['bs_gray_100_desc'] = 'Tom de cinza mais claro';
+$string['bs_gray_1100'] = 'Cinza 1100';
+$string['bs_gray_1100_desc'] = 'Tom de cinza próximo ao preto';
+$string['bs_gray_200'] = 'Cinza 200';
+$string['bs_gray_200_desc'] = 'Tom de cinza muito claro';
+$string['bs_gray_300'] = 'Cinza 300';
+$string['bs_gray_300_desc'] = 'Tom de cinza claro';
+$string['bs_gray_400'] = 'Cinza 400';
+$string['bs_gray_400_desc'] = 'Tom de cinza médio-claro';
+$string['bs_gray_500'] = 'Cinza 500';
+$string['bs_gray_500_desc'] = 'Tom de cinza neutro';
+$string['bs_gray_600'] = 'Cinza 600';
+$string['bs_gray_600_desc'] = 'Tom de cinza médio-escuro';
+$string['bs_gray_700'] = 'Cinza 700';
+$string['bs_gray_700_desc'] = 'Tom de cinza escuro';
+$string['bs_gray_800'] = 'Cinza 800';
+$string['bs_gray_800_desc'] = 'Tom de cinza mais escuro';
+$string['bs_gray_900'] = 'Cinza 900';
+$string['bs_gray_900_desc'] = 'Tom de cinza quase preto';
+$string['bs_link_color'] = 'Cor dos links';
+$string['bs_link_color_desc'] = 'Cor padrão dos links';
+$string['bs_link_focus_color'] = 'Cor dos links em foco';
+$string['bs_link_focus_color_desc'] = 'Cor utilizada quando os links recebem foco';
+$string['bs_link_hover_color'] = 'Cor dos links ao passar o mouse';
+$string['bs_link_hover_color_desc'] = 'Cor utilizada ao passar o mouse sobre os links';
+$string['bs_nav_drawer'] = 'Cor do menu de navegação';
+$string['bs_nav_drawer_desc'] = 'Cor de fundo do menu de navegação';
+$string['bs_primary'] = 'Cor primária';
+$string['bs_primary_desc'] = 'Escolha a cor primária do tema. Essa cor será utilizada em botões, links e outros elementos com destaque visual.';
+$string['bs_text_color'] = 'Cor do texto';
+$string['bs_text_color_desc'] = 'Cor padrão do texto';
+$string['bs_white'] = 'Cor branca';
+$string['bs_white_desc'] = 'Cor utilizada nos elementos brancos';
+$string['darklabel'] = 'Escuro';
+$string['darkmode'] = 'Modo escuro';
+$string['enable'] = 'Ativar';
+$string['enable_desc'] = 'Ative ou desative o plugin Boost Dark.';
+$string['lightlabel'] = 'Claro';
+$string['lightmode'] = 'Modo claro';
+$string['modulename'] = 'Boost Dark';
+$string['pluginname'] = 'Boost Dark';
+$string['privacy:metadata'] = 'Este plugin não coleta, armazena ou compartilha nenhum dado pessoal dos estudantes.';
+$string['reset_colors_to_default'] = 'Para restaurar as cores padrão, clique <a href="{$a}">aqui</a>';
